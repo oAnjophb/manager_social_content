e68524b7532e77ce9971e7ab4dcdf9d069f64986
oAnjoPHB <anjosruanp@gmail.com>
2026-06-22 19:57:35 -0300

    fix: remove unused eslint-disable comment for unbound-method in test files feat: add test for registered content status in RegisterContentUseCase

--- test/application/list-contents-use-case.spec.ts
+++ test/application/list-contents-use-case.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/unbound-method */
 import { mock, type MockProxy } from 'vitest-mock-extended'
 
 import { DataBaseConnectionError } from '#src/application/erros/database-connection-error'
